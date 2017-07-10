3d78480ff219b4535366be26c4baa173aaef7c78
JohnnyHsu <gogameboy11@gmail.com>
2017-07-10 10:18:59 +0800

    change test

--- blog/blog_site/tests.py
+++ blog/blog_site/tests.py
@@ -11,16 +11,16 @@ class ArticleTestClass(TestCase):
     #@mock.patch(blog_site.)
     def setUp(self):
         temp_article = Article.objects.create(title='temp_article', content='Ha Ha Ha!')
-        temp_article.save()
+        new_article = temp_article.save()
 
         temp_tag = Tag.objects.create(name='temp_tag')
-        temp_tag.save()
+        temp_tag = temp_tag.save()
         
-        #temp_cat = Category.objects.create(name='temp_cat')
-        #temp_cat.save()
+        temp_cat = Category.objects.create(name='temp_cat')
+        temp_cat = temp_cat.save()
 
         temp_article.tags.add(temp_tag)
-        #temp_article.category.add(temp_cat)
+        temp_article.category.add(temp_cat)
 
     def test_article(self):
         temp_article = Article.objects.get(title='temp_article')
@@ -28,3 +28,4 @@ class ArticleTestClass(TestCase):
 
         for tag in temp_article.tags.all:
             self.assertEqual(tag.name, 'temp_tag')
+        
